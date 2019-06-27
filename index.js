dde9488edf10b0d7762d9df19375e0b2892071f5
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-27 22:15:27 +0000

    Updated recursive function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let sum = 0;
+  for (const item of array) {
+    if (Array.isArray(item) === true) {
+      sum += sumItems(item);
+    } else  {
+      sum += parseInt(item);
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
